98625d413e339f6610d00bd4cacbfa0fa23441fb
evaldo <evaldo.junior@etec.sp.gov.br>
2023-04-04 13:38:07 -0300

    .

--- POO/exercicio/pokemon.php
+++ POO/exercicio/pokemon.php
@@ -4,6 +4,7 @@ class Pokemon {
     public $nome;
     public $total;
     
+    
     public $level;
     public $atacks; // Lista de ataques que o pockemon;
 
@@ -11,10 +12,12 @@ class Pokemon {
     function atack($oponente,$atack){
       
       $critical = $atack->critical();
-      $power = ;
+      $power = ?;
       $dano = ((($critical*$this->level*2)/5)+2)  
     }
 
 
 
 }
+
+//https://bulbapedia.bulbagarden.net/wiki/Damage#Damage_calculation
