5b0d668679277fcb0a97ff951b093d166ef56087
Evaldo <evaldo.junior@etec.sp.gov.br>
2023-05-16 13:05:11 -0300

    Seleção de produtos por marca.

--- POO/cosmeticos/index.php
+++ POO/cosmeticos/index.php
@@ -1,17 +1,27 @@
 <?php
    require('produtos_class.php');
-   $brand_selecionado = null;
+   $brand_selecionado = @$_GET['brand']; //1 <<<< Esta vai ser a variável que 
+   // que irá armazenar a marca escolhida
+
+   //4 <<< Obtendo o parametro brand, a partir da requisição. O @ evita erros caso o parametro não
+   //tiver sido informado
+   
+
    $json = file_get_contents('produtos.json');
    $json_data = json_decode($json,true);
    $produtos = [];
    $marcas   = []; // vetor de marcas de maquiagem
    foreach($json_data as $pro) {
-       $produto = new Product();
-       $produto->fromJson($pro);
-       array_push($produtos, $produto);
-       if (!in_array($produto->brand,$marcas)) {
-         array_push($marcas,$produto->brand);
-       }
+        $produto = new Product();
+        $produto->fromJson($pro);
+        if ($produto->brand==$brand_selecionado) { //5 - Verifica SE o produto tem a marca selecionada
+            array_push($produtos, $produto); // e só assim adiciona no vertor de produtos
+        }
+        if (!in_array($produto->brand,$marcas)) { 
+            
+            array_push($marcas,$produto->brand);
+        }
+        
    }
    sort($marcas); // ordena o vetor 
    
@@ -43,9 +53,14 @@
 <body>
     <div class="container">
        <div>  
-          <select>
+
+        <form>
+
+          <select name="brand" onchange="form.submit();"> <!-- 3 <<<< -->
               <option>Todas</option>
-              
+        <!-- form.submit é um método do framwork form do javascript que permite,entre
+        outras coisas, realizar uma requisição com os dados do formuário como se eu 
+        estivesse clicando no botão de "enviar' -->
         <?php
          foreach($marcas as $marca) {
             echo  "<option>$marca</option>";
@@ -53,17 +68,25 @@
         ?>      
 
           </select>  
+        </form>
        </div> 
    <?php
-    
-        foreach($produtos as $produto) {
-            echo "<div class='anuncio'>".
-            "<img src='$produto->image_link'>".
-            "<p>$produto->name".
-            "<p>$produto->brand".
-            "<p>$produto->price".
-            "</div>";
+      if ($brand_selecionado!=null)  { //2- Se não estiver nullo,
+                                       //ou seja, se a marca foi escolhida, 
+                                       // mostre os produtos
+            foreach($produtos as $produto) {
+                echo "<div class='anuncio'>".
+                "<img src='$produto->image_link'>".
+                "<p>$produto->name".
+                "<p>$produto->brand".
+                "<p>$produto->price".
+                "</div>";
+            }
+        } else { //3- caso contrário - Mostre uma mensagem para o usuário selecionar uma marca
+           echo "<h1>Selecione uma marca</h1>";
         }
+    
+
     
    ?>
    </ul>
